d49996bb0c014564f7f9f6b83d822582098dcca2
toon <tk.ano@outlook.be>
2016-06-14 23:39:43 +0200

    add email shroud script and make a basic contact page

---
+++ js/email.shroud.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function(){
+	var username = "tinepiano";
+	var hostname = "hotmail.com";
+	var linktext = username + "@" + hostname ;
+	$('.emailshroud').html("<a href='" + "mail" + "to:" + username + "@" + hostname + "'>" + linktext + "</a>");
+});

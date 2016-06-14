e92fbf85cceaf8d79e774815a7abcafa3b0bbc36
toon <tk.ano@outlook.be>
2016-06-14 22:23:01 +0200

    first website pages

---
+++ js/_vti_cnf/bootstrap.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|22 May 2016 11:33:30 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|LAPTOP-TOON\\Toon
+vti_modifiedby:SR|LAPTOP-TOON\\Toon
+vti_timecreated:TR|22 May 2016 09:26:04 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|22 May 2016 11:33:30 -0000
+vti_filesize:IR|69214

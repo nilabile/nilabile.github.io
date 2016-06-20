76aea25975fe599441595b4b94be622199da5f65
toon <tk.ano@outlook.be>
2016-06-20 22:49:47 +0200

    add fillpage script to fill the page when this one is not full height

---
+++ js/fillpage.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function(){
+	fillpage();
+});
+
+function fillpage()
+{
+	var containerheight=$('.container-fluid').height();
+	var windowheight=$(window).height();
+	
+	if(containerheight<windowheight)
+	{
+		$('.container-fluid').height(windowheight);
+	}
+}

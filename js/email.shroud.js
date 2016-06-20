1334dae239ed648d02e6daeb5edb3f903a93b720
toon <tk.ano@outlook.be>
2016-06-20 21:50:55 +0200

    change contact email address to info@nilabile.be

--- js/email.shroud.js
+++ js/email.shroud.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function(){
-	var username = "tinepiano";
-	var hostname = "hotmail.com";
+	var username = "info";
+	var hostname = "nilabile.be";
 	var linktext = username + "@" + hostname ;
 	$('.emailshroud').html("<a href='" + "mail" + "to:" + username + "@" + hostname + "'>" + linktext + "</a>");
 });

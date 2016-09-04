0786d76df6551eb188391ae427c99d4580816df9
nilabile <tk.ano@outlook.be>
2016-09-04 14:23:12 +0200

    fix bug in email shroud

--- js/email.shroud.js
+++ js/email.shroud.js
@@ -2,5 +2,5 @@
 	var username = "info";
 	var hostname = "nilabile.be";
 	var linktext = username + "@" + hostname ;
-	$('.emailshroud').html("<a href='" + "mail" + "to:" + username + "@" + hostname + "&bcc=tine.dekoninck@telenet.be'>" + linktext + "</a>");
+	$('.emailshroud').html("<a href='" + "mail" + "to:" + username + "@" + hostname + "?bcc=tine.dekoninck@telenet.be'>" + linktext + "</a>");
 });
